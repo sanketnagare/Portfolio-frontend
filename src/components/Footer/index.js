f39bf5e7dd29551d85d1587251b6a4e8426cd399
unknown <sanket.nagare@kanakasoftware.com>
2024-01-17 14:31:10 +0530

    Final commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Sanket nagare</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
